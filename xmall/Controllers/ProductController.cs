0323e449e1938062182a9a52549cb69065bbb9f6
xchet <chetam.okafor@gmail.com>
2019-08-26 13:11:32 +0100

    first commit

---
+++ xmall/Controllers/ProductController.cs
@@ -0,0 +1,165 @@
+﻿using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using xmall.Models;
+
+namespace xmall.Controllers
+{
+    public class ProductController : Controller
+    {
+        private xMallDBEntities db = new xMallDBEntities();
+        // GET: Product
+        public ActionResult Index()
+        {
+            var products = db.Products.Include(p => p.Batch).Include(p => p.Category).Include(p => p.Color).Include(p => p.Model).Include(p => p.Price).Include(p => p.ProductType).Include(p => p.Storage).Include(p => p.Supplier).Include(p => p.User);
+            return View(products.ToList());
+        }
+
+        public PartialViewResult ProductListPartial(int? page)
+        {
+            var pageNumber = page ?? 1;
+            var pageSize = 10;
+
+            var productList = db.Products.OrderByDescending(x => x.ProductId).ToPagedList(pageNumber, pageSize);
+            return PartialView(productList);
+        }
+
+        // GET: Product/Details/5
+        public ActionResult Details(int id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // GET: Product/Create
+        public ActionResult Create()
+        {
+            ViewBag.BatchId = new SelectList(db.Batches, "BatchId", "BatchCode");
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "Name");
+            ViewBag.ColorId = new SelectList(db.Colors, "ColorId", "Color1");
+            ViewBag.ModelId = new SelectList(db.Models, "ModelId", "Model1");
+            ViewBag.ProductTypeId = new SelectList(db.ProductTypes, "ProductTypeId", "ProductTypeName");
+            ViewBag.StorageId = new SelectList(db.Storages, "StorageId", "Storage1");
+            ViewBag.SupplierId = new SelectList(db.Suppliers, "SupplierId", "SupplierName");
+            ViewBag.UserId = new SelectList(db.Users, "UserId", "Username");
+            return View();
+        }
+
+        // POST: Product/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ProductId,ProductCode,ProductName,Image,Price,UserId,CategoryId,ColorId,ModelId,StorageId,SupplierId,BatchId,SellStartDate,SellEndDtae,IsNew,PurchasePrice,weigth,ProductUrl,ProductTypeId,Quantity")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Products.Add(product);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.BatchId = new SelectList(db.Batches, "BatchId", "BatchCode", product.BatchId);
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "Name", product.CategoryId);
+            ViewBag.ColorId = new SelectList(db.Colors, "ColorId", "Color1", product.ColorId);
+            ViewBag.ModelId = new SelectList(db.Models, "ModelId", "Model1", product.ModelId);
+            ViewBag.ProductTypeId = new SelectList(db.ProductTypes, "ProductTypeId", "ProductTypeName", product.ProductTypeId);
+            ViewBag.StorageId = new SelectList(db.Storages, "StorageId", "Storage1", product.StorageId);
+            ViewBag.SupplierId = new SelectList(db.Suppliers, "SupplierId", "SupplierName", product.SupplierId);
+            ViewBag.UserId = new SelectList(db.Users, "UserId", "Username", product.UserId);
+            return View(product);
+        }
+
+        // GET: Product/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.BatchId = new SelectList(db.Batches, "BatchId", "BatchCode", product.BatchId);
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "Name", product.CategoryId);
+            ViewBag.ColorId = new SelectList(db.Colors, "ColorId", "Color1", product.ColorId);
+            ViewBag.ModelId = new SelectList(db.Models, "ModelId", "Model1", product.ModelId);
+            ViewBag.ProductTypeId = new SelectList(db.ProductTypes, "ProductTypeId", "ProductTypeName", product.ProductTypeId);
+            ViewBag.StorageId = new SelectList(db.Storages, "StorageId", "Storage1", product.StorageId);
+            ViewBag.SupplierId = new SelectList(db.Suppliers, "SupplierId", "SupplierName", product.SupplierId);
+            ViewBag.UserId = new SelectList(db.Users, "UserId", "Username", product.UserId);
+            return View(product);
+        }
+
+        // POST: Product/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ProductId,ProductCode,ProductName,Image,Price,UserId,CategoryId,ColorId,ModelId,StorageId,SupplierId,BatchId,SellStartDate,SellEndDtae,IsNew,PurchasePrice,weigth,ProductUrl,ProductTypeId,Quantity")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(product).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.BatchId = new SelectList(db.Batches, "BatchId", "BatchCode", product.BatchId);
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "Name", product.CategoryId);
+            ViewBag.ColorId = new SelectList(db.Colors, "ColorId", "Color1", product.ColorId);
+            ViewBag.ModelId = new SelectList(db.Models, "ModelId", "Model1", product.ModelId);
+            ViewBag.ProductTypeId = new SelectList(db.ProductTypes, "ProductTypeId", "ProductTypeName", product.ProductTypeId);
+            ViewBag.StorageId = new SelectList(db.Storages, "StorageId", "Storage1", product.StorageId);
+            ViewBag.SupplierId = new SelectList(db.Suppliers, "SupplierId", "SupplierName", product.SupplierId);
+            ViewBag.UserId = new SelectList(db.Users, "UserId", "Username", product.UserId);
+            return View(product);
+        }
+
+        // GET: Product/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Product product = db.Products.Find(id);
+            db.Products.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ xmall/Controllers/ProductsController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using xmall.Models;
+
+namespace xmall.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private xMallDBEntities db = new xMallDBEntities();
+
+        // GET: Products
+        public ActionResult Index()
+        {
+            var products = db.Products.Include(p => p.Batch).Include(p => p.Category).Include(p => p.Color).Include(p => p.Model).Include(p => p.Price).Include(p => p.ProductType).Include(p => p.Storage).Include(p => p.Supplier).Include(p => p.User);
+            return View(products.ToList());
+        }
+
+        // GET: Products/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // GET: Products/Create
+        public ActionResult Create()
+        {
+            ViewBag.BatchId = new SelectList(db.Batches, "BatchId", "BatchCode");
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "Name");
+            ViewBag.ColorId = new SelectList(db.Colors, "ColorId", "Color1");
+            ViewBag.ModelId = new SelectList(db.Models, "ModelId", "Model1");
+            ViewBag.ProductPrice = new SelectList(db.Prices, "PricesId", "PricesId");
+            ViewBag.ProductTypeId = new SelectList(db.ProductTypes, "ProductTypeId", "ProductTypeName");
+            ViewBag.StorageId = new SelectList(db.Storages, "StorageId", "Storage1");
+            ViewBag.SupplierId = new SelectList(db.Suppliers, "SupplierId", "SupplierName");
+            ViewBag.UserId = new SelectList(db.Users, "UserId", "Username");
+            return View();
+        }
+
+        // POST: Products/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ProductId,ProductCode,ProductName,ProductImage,UserId,CategoryId,ColorId,ModelId,StorageId,SupplierId,BatchId,SellStartDate,SellEndDtae,IsNew,weigth,ProductUrl,ProductTypeId,ProductPrice")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Products.Add(product);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.BatchId = new SelectList(db.Batches, "BatchId", "BatchCode", product.BatchId);
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "Name", product.CategoryId);
+            ViewBag.ColorId = new SelectList(db.Colors, "ColorId", "Color1", product.ColorId);
+            ViewBag.ModelId = new SelectList(db.Models, "ModelId", "Model1", product.ModelId);
+            ViewBag.ProductPrice = new SelectList(db.Prices, "PricesId", "PricesId", product.ProductPrice);
+            ViewBag.ProductTypeId = new SelectList(db.ProductTypes, "ProductTypeId", "ProductTypeName", product.ProductTypeId);
+            ViewBag.StorageId = new SelectList(db.Storages, "StorageId", "Storage1", product.StorageId);
+            ViewBag.SupplierId = new SelectList(db.Suppliers, "SupplierId", "SupplierName", product.SupplierId);
+            ViewBag.UserId = new SelectList(db.Users, "UserId", "Username", product.UserId);
+            return View(product);
+        }
+
+        // GET: Products/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.BatchId = new SelectList(db.Batches, "BatchId", "BatchCode", product.BatchId);
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "Name", product.CategoryId);
+            ViewBag.ColorId = new SelectList(db.Colors, "ColorId", "Color1", product.ColorId);
+            ViewBag.ModelId = new SelectList(db.Models, "ModelId", "Model1", product.ModelId);
+            ViewBag.ProductPrice = new SelectList(db.Prices, "PricesId", "PricesId", product.ProductPrice);
+            ViewBag.ProductTypeId = new SelectList(db.ProductTypes, "ProductTypeId", "ProductTypeName", product.ProductTypeId);
+            ViewBag.StorageId = new SelectList(db.Storages, "StorageId", "Storage1", product.StorageId);
+            ViewBag.SupplierId = new SelectList(db.Suppliers, "SupplierId", "SupplierName", product.SupplierId);
+            ViewBag.UserId = new SelectList(db.Users, "UserId", "Username", product.UserId);
+            return View(product);
+        }
+
+        // POST: Products/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ProductId,ProductCode,ProductName,ProductImage,UserId,CategoryId,ColorId,ModelId,StorageId,SupplierId,BatchId,SellStartDate,SellEndDtae,IsNew,weigth,ProductUrl,ProductTypeId,ProductPrice")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(product).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.BatchId = new SelectList(db.Batches, "BatchId", "BatchCode", product.BatchId);
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "Name", product.CategoryId);
+            ViewBag.ColorId = new SelectList(db.Colors, "ColorId", "Color1", product.ColorId);
+            ViewBag.ModelId = new SelectList(db.Models, "ModelId", "Model1", product.ModelId);
+            ViewBag.ProductPrice = new SelectList(db.Prices, "PricesId", "PricesId", product.ProductPrice);
+            ViewBag.ProductTypeId = new SelectList(db.ProductTypes, "ProductTypeId", "ProductTypeName", product.ProductTypeId);
+            ViewBag.StorageId = new SelectList(db.Storages, "StorageId", "Storage1", product.StorageId);
+            ViewBag.SupplierId = new SelectList(db.Suppliers, "SupplierId", "SupplierName", product.SupplierId);
+            ViewBag.UserId = new SelectList(db.Users, "UserId", "Username", product.UserId);
+            return View(product);
+        }
+
+        // GET: Products/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Product product = db.Products.Find(id);
+            db.Products.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
